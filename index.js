655fe32968d05111b9810cbda3f86b470728f8d9
+ <+>
2022-08-29 12:49:43 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+
 // code your solution here
+
+const record = [
+    { year: "2015", result: "Win"},
+    { year: "2014", result: "Loss"}
+];
+
+function superbowlWin(team){
+        return team.result === "Win";
+
+}
+return (record.find(superbowlWin));
